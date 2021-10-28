e4bc8e04639a729457b7c6ae13ff58f5f1c7c9e3
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-10-28 07:34:45 -0500

    Questions for Manager, Engineer, and Intern are built out on index.js

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
-console.log("Hello World!");
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the manager\'s name?',
+            name: 'manager-name'
+        },
+        {
+            type: 'input',
+            message: 'What is the manager\'s ID?',
+            name: 'manager-id'
+        },
+        {
+            type: 'input',
+            message: 'What is the manager\'s email address?',
+            name: 'manager-email'
+        },
+        {
+            type: 'input',
+            message: 'What is the manager\'s office number?',
+            name: 'manager-office-number'
+        },
+
+        // Here add the menu to add an engineer or intern
+
+        // Questions for Engineer
+        {
+            type: 'input',
+            message: 'What is the engineer\'s name?',
+            name: 'engineer-name'
+        },
+        {
+            type: 'input',
+            message: 'What is the engineer\'s ID?',
+            name: 'engineer-id'
+        },
+        {
+            type: 'input',
+            message: 'What is the engineer\'s email address?',
+            name: 'engineer-email'
+        },
+        {
+            type: 'input',
+            message: 'What is the engineer\'s Github username?',
+            name: 'engineer-github-username'
+        },
+
+        // Intern questions
+
+        {
+            type: 'input',
+            message: 'What is the intern\'s name?',
+            name: 'intern-name'
+        },
+        {
+            type: 'input',
+            message: 'What is the intern\'s ID?',
+            name: 'intern-id'
+        },
+        {
+            type: 'input',
+            message: 'What is the intern\'s email address?',
+            name: 'intern-email'
+        },
+        {
+            type: 'input',
+            message: 'What is the intern\'s office number?',
+            name: 'intern-school'
+        },
+
+        // Return to menu 
+
+
+        ])    
+    };
+        
