4d07677337d07015e52030980f1d6a4df18d93b1
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-10-29 19:46:03 -0500

    Changed structure of prompts to be all individual objects with promptContinue function

--- index.js
+++ index.js
@@ -1,91 +1,102 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
 
-const promptUser = () => {
+const promptManager = () => {
     return inquirer.prompt([
         {
             type: 'input',
             message: 'What is the manager\'s name?',
-            name: 'manager-name'
+            name: 'managerName',
         },
         {
             type: 'input',
             message: 'What is the manager\'s ID?',
-            name: 'manager-id'
+            name: 'managerId',
         },
         {
             type: 'input',
             message: 'What is the manager\'s email address?',
-            name: 'manager-email'
+            name: 'managerEmail',
         },
         {
             type: 'input',
             message: 'What is the manager\'s office number?',
-            name: 'manager-office-number'
+            name: 'managerOfficeNumber',
         },
+    ])
+    .then((response) => {
+        employeeList.push(new Manager(response.name, response.id, response.email, response.officeNumber));
+        console.log('Manager successfully added to the team.');
+        promptContinue();
+    })
+};
 
-        {
-            type: 'list',
-            message: 'Who else is on the team?',
-            name: 'add-member',
-            choices: ['Engineer', 'Intern'],
-            when: function(answers) {
-                return answers.add-member
-            }
-        },
-
-        // Here add the menu to add an engineer or intern
+const promptEngineer = () => {
+    return inquirer.prompt([
 
         // Questions for Engineer
         {
             type: 'input',
             message: 'What is the engineer\'s name?',
-            name: 'engineer-name'
+            name: 'engineerName'
         },
         {
             type: 'input',
             message: 'What is the engineer\'s ID?',
-            name: 'engineer-id'
+            name: 'engineerId'
         },
         {
             type: 'input',
             message: 'What is the engineer\'s email address?',
-            name: 'engineer-email'
+            name: 'engineerEmail'
         },
         {
             type: 'input',
             message: 'What is the engineer\'s Github username?',
-            name: 'engineer-github-username'
+            name: 'engineerGithubUsername'
         },
+    ])
+        .then((response) => {
+            employeeList.push(new Engineer(response.name, response.id, response.email, response.github));
+            console.log('Intern successfully added to the team.');
+            promptContinue();
+        })
+}
 
         // Intern questions
+const promptIntern = () => {
+    return inquirer.prompt([
 
         {
             type: 'input',
             message: 'What is the intern\'s name?',
-            name: 'intern-name'
+            name: 'internName'
         },
         {
             type: 'input',
             message: 'What is the intern\'s ID?',
-            name: 'intern-id'
+            name: 'internId'
         },
         {
             type: 'input',
             message: 'What is the intern\'s email address?',
-            name: 'intern-email'
+            name: 'internEmail'
         },
         {
             type: 'input',
             message: 'What is the intern\'s office number?',
-            name: 'intern-school'
+            name: 'internSchool'
         },
-
-        // Return to menu 
-
-
-        ])    
-    };
+    ])
+        .then((response) => {
+            employeeList.push(new Intern(response.name, response.id, response.email, response.school));
+            console.log('Intern successfully added to the team.');
+            promptContinue();
+        })
+};
 
 
 const generateHTML = ({ managerName, managerId, managerEmail, managerOfficeNumber, engineerName, engineerId, engineerEmail, engineerGithubUsername, internName, internId, internEmail, internSchool }) =>
@@ -109,11 +120,34 @@ const generateHTML = ({ managerName, managerId, managerEmail, managerOfficeNumbe
     
 // Bonus using writeFileSync as a promise
 const init = () => {
-    promptUser()
+    promptManager()
     // Use writeFileSync method to use promises instead of a callback function
-    .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
+    .then((response) => fs.writeFileSync('index.html', generateHTML(response)))
     .then(() => console.log('Successfully wrote to index.html'))
     .catch((err) => console.error(err));
 };
-    
+
 init();
+
+function promptContinue() {
+    console.log('Select the type of employee: ')
+    inquirer.prompt([
+        {
+            type: 'list',
+            choices: ['Engineer', 'Intern', 'Finish building team'],
+            name: 'selection',
+        },
+    ])
+    .then((response) => {
+        if(response.selection === 'Engineer') {
+            promptEngineer();
+        } else if (response.selection === 'Intern') {
+            promptIntern();
+        } else {
+            console.log('Your team is being generated!');
+            // call function to get to generating the html page
+            generateHTML.generateText(employeeList, employeeList.length);
+        }
+    })
+}
+
