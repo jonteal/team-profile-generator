7c841cebe07926942c510dd92b29664c69322a0e
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-10-31 19:01:26 -0500

    Moved assets folder. Initiated generate HTML file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
+const Employee = require('./lib/Employee');
 
 const employeeList = [];
 
